bff0fe034128323d6937ce0a668f8bf5620b6a08
Esteban <acevedoesteban999@gmail.com>
2025-12-08 12:26:14 -0500

    [UPD] manifest version

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Activity DateTime',
-    'version': '19.0.0.1',
+    'version': '19.1.0.0',
     'summary': """ Activity DateTime """,
     'author': 'acevedoesteban999@gmail.com , esteban.acevedo@qa-bit.com',
     'website': 'https://acevedoesteban999/eActivityDatetime',
