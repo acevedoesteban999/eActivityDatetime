93d6163dcc21cfb353028b389e8d211c2fb24f72
estebanACB2 <esteban.acevedo@qa-bit.com>
2025-12-09 22:53:47 -0500

    [FIX] Fix date's to eremaining_days_datetime widget

--- static/src/components/remaining_days_datetime/remaining_days_datetime_field.js
+++ static/src/components/remaining_days_datetime/remaining_days_datetime_field.js
@@ -1,23 +1,78 @@
 /** @odoo-module **/
 
 import { registry } from "@web/core/registry";
+import { evaluateExpr } from "@web/core/py_js/py";
+import { getClassNameFromDecoration } from "@web/views/utils";
 import { RemainingDaysField , remainingDaysField } from "@web/views/fields/remaining_days/remaining_days_field";
+import { formatDate } from "@web/views/fields/formatters";
+import { capitalize } from "@web/core/utils/strings";
+const { DateTime } = luxon;
 
 export class ERemainingDaysDatetime extends RemainingDaysField {
     setup() {
+      this.props.classes.danger = "days < 0 or (days == 0 and passEventTime)";
+      this.props.classes.warning = "days == 0 and not passEventTime";
     }
     
+    get diffDays() {
+      if(!this.props.record.data.all_day){
+        const today = DateTime.local().startOf("day");
+        const diff = this.props.record.data.datetime_deadline.startOf("day").diff(today, "days");
+        return Math.floor(diff.days);
+      }
+      return super.diffDays   
+    }
+
+    get classNames() {
+      if(!this.props.record.data.all_day){
+        if (this.diffDays === null) {
+          return null;
+        }
+        if (!this.props.record.isActive) {
+            return null;
+        }
+        const classNames = {};
+        const evalContext = { days: this.diffDays, record: this.props.record.evalContext,passEventTime: this.props.record.data.datetime_deadline < DateTime.now() };
+        for (const decoration in this.props.classes) {
+            const value = evaluateExpr(this.props.classes[decoration], evalContext);
+            classNames[getClassNameFromDecoration(decoration)] = value;
+        }
+        return classNames;
+      }
+      return super.classNames
+      
+    }
+
     get dateTimeValue(){
       if(!this.props.record.data.all_day && Math.abs(this.diffDays) <= 2)
         return " (" + this.props.record.data.datetime_deadline.toFormat('HH:mm') + ")";
       return ""
     }
 
+    get formattedValue() {
+      if(!this.props.record.data.all_day){
+          return formatDate(this.props.record.data.datetime_deadline, { numeric: true });
+      }
+      return super.formattedValue
+    }
+
     get diffString() {
-        return super.diffString + this.dateTimeValue;  
+      if(!this.props.record.data.all_day){
+        if (this.diffDays === null) {
+            return "";
+        }
+        if (Math.abs(this.diffDays) > 99) {
+            return this.formattedValue;
+        }
+        return capitalize(this.props.record.data.datetime_deadline.toRelativeCalendar()) + this.dateTimeValue;
+      }
+      return super.diffString + this.dateTimeValue;  
     }
 
     get numericValue() {
+      if(!this.props.record.data.all_day){
+        return formatDate(this.props.record.data.datetime_deadline, { numeric: true }) + this.dateTimeValue;
+      }
       return super.numericValue + this.dateTimeValue
     }
 }
